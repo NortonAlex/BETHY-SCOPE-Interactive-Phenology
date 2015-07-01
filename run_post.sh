91fd4517ce5d59af24ddff901f4f4e26526584d6
Alex Norton <nortona@student.unimelb.edu.au>
2015-07-01 14:15:18 +1000

    Added run_post shell script
    Created a shell script, run_post.sh, that defines how many OMP threads
    to use during parallelization. However, checking the run at this stage
    has shown that SCOPE fluo and gpp are not output properly, all zero in
    output files.

---
+++ run_post.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export OMP_NUM_THREADS=4
+## lift any arbitrary limits on the per-process stack-size
+#ulimit -s unlimited
+## raise the limit for the OpenMP per-thread stack-size
+#export OMP_STACKSIZE=100MB
+
+./post
+
