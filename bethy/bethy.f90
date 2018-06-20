03eefb73018bc393e66dcc2a737423dbc1380805
Alex Norton <nortona@student.unimelb.edu.au>
2018-06-20 14:38:53 +1000

    Setup for oco2 sif assimilation
    Jacobian runs for the oco2 sif assimilation were run using this commit.
    This is for the Biogeosciences Discussions paper
    doi: 10.5194/bg-2018-270.

--- bethy/bethy.f90
+++ bethy/bethy.f90
@@ -177,7 +177,7 @@ print *, 'tdays =', tdays
 
         ! Option to use monthly prescribed LAI.
         !  - lai is forced for simulated period, not spin-up.
-        !IF (rday > sdays) lai = prescribed_lai(:,rmonth)
+        IF (rday > sdays) lai = prescribed_lai(:,rmonth)
 
         IF (rday == idayint(rday)) THEN
            ryear0 = outyear
@@ -254,18 +254,18 @@ print *, 'tdays =', tdays
                    & EC,EO,EV,ER,EK,tgam,alpha,alc4,kc0,ko0,zgrowth,zmaint)
               ! .. do diurnal diagnostics 
               ! option to give prescribed lai (from file) to fluorescence calculations
-!              IF ( inho == 13 ) THEN
+              IF ( inho == 13 ) THEN
               CALL fluorescence (ryear,rmonth,iday,inho,iday0,iday1,swdown,pardown,&
                                 & tmp(inho,:),pair,eamin,ca,OX, & 
                                 & zlai, &
-                                & jmf,vm,EC,EO,EV,ER,EK,kc0,ko0,vomf,rdf,&
+                                & jmf,vms,EC,EO,EV,ER,EK,kc0s,ko0s,vomf,rdf,&
                                 & rfluo,rgppfluo,PAR_scope,PAR_scope_cab,&
                                 & rfluo_diurnal,rgppfluo_diurnal,&
                                 & rlai_diurnal,rapar_diurnal,raparcab_diurnal,rpar_diurnal)             
               zassc = zgppfluo               ! ANorton. To allow SCOPE-GPP to pass onto subsequent c-balance equations
 !              print *,'SCOPE FLUO::', rfluo
 !              print *,'SCOPE GPP::', rgppfluo
-!              ENDIF         ! for selected time of fluo computation
+              ENDIF         ! for selected time of fluo computation
               CALL diagnostics (ng,vp,zassc,zraut,zgrowth,zmaint,ztrans,zptrans,zpcevp,zpsevp)
 
 	   ENDDO ! end diurnal timestep loop 
