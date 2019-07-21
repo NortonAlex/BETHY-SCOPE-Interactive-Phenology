f700d86c41388174168dc3f1737b79b84b316dee
Alex Norton <nortona@student.unimelb.edu.au>
2019-07-22 00:29:29 +1000

    Missed this file on previous commit

--- bethy/integration.f90
+++ bethy/integration.f90
@@ -38,6 +38,8 @@ REAL, INTENT(OUT), DIMENSION(npt)                :: Fout
 
 ! Local variables 
 REAL, DIMENSION(nli,nlazi,nl)                    :: Fout0 
+REAL, DIMENSION(nlazi,nl)                        :: Fout1
+REAL, DIMENSION(nl)                              :: Fout2
 INTEGER                                          :: j 
 
 !Fout = zeros(nli, nlazi,nl);
@@ -49,15 +51,25 @@ SELECT CASE (trim(choice))
 !%% integration over leaf angles
 ! Not test for the moment ... EK 5/03/2013
     CASE ('angles')
+        print*,'integrating over angles'
         DO j =1, nli 
              Fout0(j,:,:)    = F(j,:,:)*lidf(j)       !  % [nli, nlazi,nl]
-       END DO  
+        END DO  
 
+        print*,'Fout0:',shape(Fout0)
     !!!    Fout            = permute(Fout,[3 1 2])  !  % [nl]
-      DO j=1,nl 
-           Fout(j) =  sum(Fout0(:,:,j))/nlazi   !  [1,1,nl]
-      END DO    
+        Fout1 = sum( Fout0, dim=1 )
+        print*,'Fout1:',shape(Fout1)
+        Fout2 = sum( Fout1, dim=1 )
+        print*,'Fout2:',shape(Fout2)
+        DO j=1,nl
+            Fout(j) = Fout2(j)/nlazi
+        END DO
+!        DO j=1,nl 
+!             Fout(j) =  sum(Fout0(:,:,j))/nlazi   !  [1,1,nl]
+!        END DO    
 
+        print*,'Fout:',shape(Fout)
 !%% integration over layers only         
    CASE ('layers')
         !%not implemented
